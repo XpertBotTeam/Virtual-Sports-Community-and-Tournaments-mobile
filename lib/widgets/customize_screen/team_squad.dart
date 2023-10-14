ad3dd468a03eb0bb31989846348d5bae794a8b04
jadantoun0 <113588273+jadantoun0@users.noreply.github.com>
2023-10-14 03:45:02 +0300

    adjusted players positioning

--- lib/widgets/customize_screen/team_squad.dart
+++ lib/widgets/customize_screen/team_squad.dart
@@ -24,7 +24,6 @@ class _TeamSquadState extends State<TeamSquad> {
   @override
   void initState() {
     super.initState();
-
     // caching images
     widget.players.forEach((player) {
       if (player.starterImage != null && !imagesCache.containsKey(player.starterImage)) {
@@ -34,37 +33,71 @@ class _TeamSquadState extends State<TeamSquad> {
     });
   }
 
+  AssetImage getImageFromThemeColor() {
+    if (widget.themeColor == 'purple') {
+      return const AssetImage("lib/assets/pitches/purple.webp") ;
+    }
+    if (widget.themeColor == 'blue') {
+      return const AssetImage("lib/assets/pitches/blue.png") ;
+    }
+    if (widget.themeColor == 'red') {
+      return const AssetImage("lib/assets/pitches/red.png");
+    }
+    else {
+      return const AssetImage("lib/assets/pitches/green.png") ;
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
+
+    var width = MediaQuery.of(context).size.width;
+    var fivePercent = MediaQuery.of(context).size.width * 0.05;
+    var boxWidth = 60;
+    var boxHeight = boxWidth + 5 + boxWidth/4 + 3 + boxWidth/4;
+    var imageAspectRatio = 763 / 964; 
+
+    final startX = fivePercent + 10; // 2px until field start
+    final endX = width - fivePercent - 10; // 2px for field
+    final startY = 0;
+    
+    // calculating endY:
+    var imageWidth = endX - startX;
+    var imageHeight = imageWidth / imageAspectRatio;
+    final endY = imageHeight;
+
+
     return Stack(
       children:[
         Opacity(
           opacity: 0.5,
           child: Container(
-            height: 450,    
+            height: 450,  
+            margin: EdgeInsets.symmetric(horizontal: fivePercent),  
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: widget.themeColor == 'green'?
-                    const AssetImage("lib/assets/pitches/green.png") : 
-                      widget.themeColor == 'purple'?
-                        const AssetImage("lib/assets/pitches/purple.webp") :
-                        widget.themeColor == 'blue'?
-                          const AssetImage("lib/assets/pitches/blue.png") :
-                          const AssetImage("lib/assets/pitches/red.png")
+                image: getImageFromThemeColor(),
+                fit: BoxFit.fitWidth
               )
             ),        
           ),
         ),
-        
-        PlayerBox(
-          player: widget.players[0], 
-          initialBoxX: 0, 
-          initialBoxY: 0, 
-          refreshSquad: refreshSquad, 
-          imagesCache: imagesCache
-        ),
-      
+        // goalkeeper  
+        PlayerBox(player: widget.players[0], initialBoxX: startX+imageWidth/2 - boxWidth/2, initialBoxY: 0, refreshSquad: refreshSquad, imagesCache: imagesCache),
+        // defenders
+        PlayerBox(player: widget.players[1], initialBoxX: startX, initialBoxY: boxHeight*1.2, refreshSquad: refreshSquad, imagesCache: imagesCache),
+        PlayerBox(player: widget.players[2], initialBoxX: startX+1.1*boxWidth, initialBoxY: boxHeight/2, refreshSquad: refreshSquad, imagesCache: imagesCache),
+        PlayerBox(player: widget.players[3], initialBoxX: endX-1.1 *2*boxWidth, initialBoxY: boxHeight/2, refreshSquad: refreshSquad, imagesCache: imagesCache),
+        PlayerBox(player: widget.players[4], initialBoxX: endX-boxWidth, initialBoxY: boxHeight*1.2, refreshSquad: refreshSquad, imagesCache: imagesCache),
+        // midfielders
+        PlayerBox(player: widget.players[5], initialBoxX: startX+imageWidth/2 - boxWidth/2, initialBoxY: 1.5*boxHeight, refreshSquad: refreshSquad, imagesCache: imagesCache),
+        PlayerBox(player: widget.players[6], initialBoxX: startX+1.1*boxWidth, initialBoxY: 2*boxHeight, refreshSquad: refreshSquad, imagesCache: imagesCache),
+        PlayerBox(player: widget.players[7], initialBoxX: endX-1.1*2*boxWidth, initialBoxY: 2*boxHeight, refreshSquad: refreshSquad, imagesCache: imagesCache),
+        // attackers
+        PlayerBox(player: widget.players[8], initialBoxX: startX, initialBoxY: endY-boxHeight*1.5, refreshSquad: refreshSquad, imagesCache: imagesCache),
+        PlayerBox(player: widget.players[9], initialBoxX: endX-boxWidth, initialBoxY: endY-boxHeight*1.5, refreshSquad: refreshSquad, imagesCache: imagesCache),
+        PlayerBox(player: widget.players[10], initialBoxX: startX+imageWidth/2 - boxWidth/2, initialBoxY: endY-boxHeight, refreshSquad: refreshSquad, imagesCache: imagesCache),
       ] 
     );
   }
